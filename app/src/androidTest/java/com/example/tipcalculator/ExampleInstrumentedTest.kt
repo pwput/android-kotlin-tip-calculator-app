bf4a8d02963111096565c0b668065223be3795a1
pwput <45051480+pwput@users.noreply.github.com>
2022-03-23 19:17:37 +0100

    tests added
    Took 56 minutes

--- app/src/androidTest/java/com/example/tipcalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tipcalculator/ExampleInstrumentedTest.kt
@@ -8,6 +8,17 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+import androidx.test.espresso.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Matchers.containsString
+import org.junit.Rule
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -15,10 +26,20 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    // geting Main Activity
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.tipcalculator", appContext.packageName)
+    }
+    @Test
+    fun calculate_20_percent_tip(){
+        onView(withId(R.id.costInput)).perform(typeText("50.00"))
+        onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.tipResult)).check(matches(withText(containsString("$10.00"))))
     }
 }
--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Views binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//button listener
+        binding.button.setOnClickListener { calculateTip() }
+
+
     }
+
+    private fun calculateTip() {
+//        getting input
+        val input = binding.costInput.text.toString().toDoubleOrNull()
+        if (input == null) {
+            binding.tipResult.text = ""
+            return
+        }
+//        getting percentage
+        val tipPercetage = when (binding.radioGroup.checkedRadioButtonId) {
+            R.id.radioAmazing -> 0.20
+            R.id.radioGood -> 0.18
+            else -> 0.15
+        }
+        //checking round up and calculating tip amount
+        var tip = input.times(tipPercetage)
+        if (binding.switchRound.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+//        formating output
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount) + formattedTip.toString()
+    }
+
 }
